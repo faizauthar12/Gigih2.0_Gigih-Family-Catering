08bda9087c928d460b325be3b24fec06ec48c449
Faiz Authar <faizauthar@gmail.com>
2022-04-20 15:15:31 +0700

    app: models: menus: Add validation for price price can't be blank price can't be lower than 0.01

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,6 @@
 class Menu < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    validates :price, presence: true, numericality: {
+        greater_than_or_equal_to: 0.01 # Price must be greater than or equal to 0.01
+    }
 end
