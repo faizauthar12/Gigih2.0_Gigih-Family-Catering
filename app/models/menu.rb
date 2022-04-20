82d9a0bb0d8935d3da11ec1b2c5c47341678e7fd
Faiz Authar <faizauthar@gmail.com>
2022-04-20 15:17:54 +0700

    app: models: menus: Add validation for description description maximum length is 150

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,8 @@
 class Menu < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    validates :description, length: {
+        maximum: 150 # Description is too long (maximum is 150 characters)
+    }
     validates :price, presence: true, numericality: {
         greater_than_or_equal_to: 0.01 # Price must be greater than or equal to 0.01
     }
