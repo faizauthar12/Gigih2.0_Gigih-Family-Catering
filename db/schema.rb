56141dd9a062c0c6d53a373b292158854649edc5
Faiz Authar <faizauthar@gmail.com>
2022-04-21 11:10:57 +0700

    generate customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_073132) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_033443) do
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.text "description"
