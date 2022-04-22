ffe9751e6cd69150d572040aed67ced11169428a
Faiz Authar <faizauthar@gmail.com>
2022-04-22 23:12:48 +0700

    db: seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Destroy all current database
+Menu.destroy_all
+Category.destroy_all
+
+# Create category
+Category1 = Category.create(name: "Makanan") # id: 1
+Category2 = Category.create(name: "Minuman") # id: 2
+
+# Create menu
+## Makanan
+Menu.create(
+    name: "Nasi Goreng",
+    description: "Kasta tertinggi nasi",
+    price: 15000.0,
+    categories: [Category1]
+)
+
+Menu.create(name: "Nasi uduk",
+    description: "Nasi uduk",
+    price: 10000.0,
+    categories: [Category1]
+)
+
+## Minuman
+Menu.create(name: "Teh",
+    description: "Teh",
+    price: 15000.0,
+    categories: [Category1]
+)
