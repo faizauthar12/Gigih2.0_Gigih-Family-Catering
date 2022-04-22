9886bb55c7f50e7293aa6b1fb2f07c7b47ea0e03
Faiz Authar <faizauthar@gmail.com>
2022-04-22 10:30:25 +0700

    Generate categories model `rails generate scaffold Category name:string`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_073132) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_032857) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.text "description"
