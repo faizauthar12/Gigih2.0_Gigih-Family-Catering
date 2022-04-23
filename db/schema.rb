1aee69f786abd716c7020afd1cd99bfc4cdeb935
Faiz Authar <faizauthar@gmail.com>
2022-04-23 11:21:55 +0700

    generate status scaffold `rails g scaffold status name:string`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_034523) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_23_041518) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_22_034523) do
     t.string "name"
     t.text "description"
     t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
